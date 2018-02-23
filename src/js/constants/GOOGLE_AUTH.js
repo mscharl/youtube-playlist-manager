b230d3ab26690d729bfecdff4da28ff20f09d90f
Michael Scharl <michael.scharl@me.com>
2018-02-23 18:49:38 +0100

    Updated Google client ID

--- src/js/constants/GOOGLE_AUTH.js
+++ src/js/constants/GOOGLE_AUTH.js
@@ -1,2 +1,2 @@
 export const API_KEY   = 'AIzaSyBmf1TFdLLbBuhxDrzQ501eiMmUxDUEPNQ';
-export const CLIENT_ID = '157581725280-pal8r42240lghpickrpmiibho75jjnam.apps.googleusercontent.com';
+export const CLIENT_ID = '157581725280-jhatlm16l7gko7f394dmkv0lvsgqr6b9.apps.googleusercontent.com';
