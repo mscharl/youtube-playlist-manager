e4a70a12c980a2ba673230739df72a34e99b672a
Michael Scharl <michael.scharl@me.com>
2018-02-23 15:55:53 +0100

    Removed strict mode in non production environment

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -8,7 +8,8 @@ import playlists from './playlists';
 Vue.use(vuex);
 
 export default new vuex.Store({
-    strict: process.env.NODE_ENV !== 'production',
+    // TODO: Strict mode causes `Maximum call stack size exceeded`
+    // strict: process.env.NODE_ENV !== 'production',
 
     modules: {
         [moduleTypes.GOOGLE_AUTH]: google_auth,
