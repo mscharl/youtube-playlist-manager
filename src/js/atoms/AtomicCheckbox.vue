f9e00c92b16163563d34c22397b290e63725379b
Michael Scharl <michael.scharl@massiveart.com>
2018-05-29 08:14:33 +0200

    Created the AtomicCheckbox component

---
+++ src/js/atoms/AtomicCheckbox.vue
@@ -0,0 +1,54 @@
+<template>
+    <label class="AtomicCheckbox" :class="classes" @click.stop>
+        <input type="checkbox" class="custom-control-input" :disabled="disabled" v-model="model" >
+        <span class="AtomicCheckbox__mark"></span>
+    </label>
+</template>
+
+<script>
+    export default {
+        name: 'AtomicCheckbox',
+
+        props: {
+            value: {
+                type: Boolean,
+                default: null,
+            },
+
+            disabled: {
+                type: Boolean,
+                default: false,
+            },
+        },
+
+        computed: {
+            model: {
+                get() {
+                    return this.value;
+                },
+
+                set(newValue) {
+                    this.$emit('input', newValue);
+                },
+            },
+
+            classes() {
+                return [
+                    {
+                        '--disabled': this.disabled,
+                    },
+                ]
+            },
+
+            id() {
+                return `atomic-checkbox--${this.uid}`;
+            },
+        },
+
+        methods: {
+            valueChanged(event) {
+                this.model = !this.model;
+            },
+        },
+    }
+</script>
