e527a8fd87f95a45dbf4a981061d851334ddc950
Michael Scharl <michael.scharl@massiveart.com>
2018-06-19 09:09:39 +0200

    Implemented atomic fixed position into playlist header

--- src/js/components/PlaylistHeader.vue
+++ src/js/components/PlaylistHeader.vue
@@ -1,11 +1,19 @@
 <template>
-    <header class="PlaylistHeader">
-        Header
-    </header>
+    <atomic-fixed-position class="PlaylistHeader__wrapper">
+        <header class="PlaylistHeader">
+            Header
+        </header>
+    </atomic-fixed-position>
 </template>
 
 <script>
+    import AtomicFixedPosition from '../atoms/AtomicFixedPosition';
+
     export default {
         name: 'PlaylistHeader',
+
+        components: {
+            AtomicFixedPosition,
+        },
     }
 </script>
