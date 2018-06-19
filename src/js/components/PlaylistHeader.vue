a9d46e68ac9dc9ddafa9d000cf290fee0576e201
Michael Scharl <michael.scharl@massiveart.com>
2018-06-19 09:11:36 +0200

    Added playlist title to the header

--- src/js/components/PlaylistHeader.vue
+++ src/js/components/PlaylistHeader.vue
@@ -1,16 +1,27 @@
 <template>
     <atomic-fixed-position class="PlaylistHeader__wrapper">
         <header class="PlaylistHeader">
-            Header
+            <h1 class="PlaylistHeader__title">{{ title }}</h1>
         </header>
     </atomic-fixed-position>
 </template>
 
 <script>
+    import { createNamespacedHelpers } from 'vuex'
     import AtomicFixedPosition from '../atoms/AtomicFixedPosition';
+    import * as storeModules from '../store/types/modules';
+    import * as videosGetters from '../store/videos/types/getters';
+
+    const { mapGetters } = createNamespacedHelpers(storeModules.VIDEOS);
 
     export default {
         name: 'PlaylistHeader',
+
+        computed: {
+            ...mapGetters({
+                title: videosGetters.PLAYLIST_TITLE,
+            }),
+        },
 
         components: {
             AtomicFixedPosition,
